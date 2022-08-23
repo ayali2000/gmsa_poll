f9000258c11a3bbb9ab81417d1b3a69e74d1cc69
Arafat Yassin Ali <aliarafatyassin@gmail.com>
2022-08-22 23:00:39 -1200

    me

--- GMSA_poll/urls.py
+++ GMSA_poll/urls.py
@@ -22,4 +22,10 @@ from django.urls import path,include
 urlpatterns = [
     path('',include('poll.url')),
     path('fuseina/', admin.site.urls),
-] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
